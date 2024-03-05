1e15177a59a8faf4af12ea858905457ae0c89cde
Efe Burak Çakıcı <92547068+efeburakcakici@users.noreply.github.com>
2024-03-05 22:01:01 +0300

    Add files via upload

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,19 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Efe Burak Çakıcı on 5.03.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+import UIKit
+
+
+
+struct BMI {
+    
+    let value : Float
+    let advice : String
+    let color : UIColor
+    
+    
+}
